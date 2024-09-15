0d940b4e91ed66da2d8e7fe44c065d32dd23680b
wambui006 <jackynjanja44@gmail.com>
2024-09-15 16:29:39 +0300

    answers to assignment task

---
+++ assignment.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def introduce(self):
+        print(f"Hello, my name is {self.name}. I am {self.age} years old and I identify as {self.gender}.")
+
+
+person1 = Person("Jackie", 23, "Female")
+
+
+person1.introduce()
